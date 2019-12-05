b8058dd20f8c0925d1ea11079c8def08e5c10d6e
fahim.hadi <fahimhadimaula@gmail.com>
2019-12-05 11:56:29 +0700

    bug: room wasn't booked

--- script/func_meetreq.py
+++ script/func_meetreq.py
@@ -5,10 +5,12 @@ import os, datetime
 import json
 import openpyxl
 import re
+import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
-from email.utils import COMMASPACE, formatdate
+from email.utils import COMMASPACE, formatdate, formataddr
+from email.header import Header
 from email import encoders
 import tkinter as tk
 from tkinter import ttk
@@ -31,12 +33,13 @@ def df_from_excel(path):
     return pd.read_excel(path,header=0)
 
 def open_exc(path):
-    global wb_main, wb_trainer, wb_trainee, wb_cc, wb_trainroom
+    global wb_main, wb_trainer, wb_trainee, wb_cc, wb_trainroom, wb_eti
     wb_main = df_from_excel(path)
     wb_trainer = pd.read_excel(path, sheet_name="trainer")
     wb_trainee = pd.read_excel(path, sheet_name="trainee")
     wb_cc = pd.read_excel(path, sheet_name="CC")
     wb_trainroom = pd.read_excel(path, sheet_name="training_room")
+    wb_eti = pd.read_excel(path, sheet_name="ETI")
 
 def meeting_req(df):
     for event in df["event_code"].unique().tolist():
@@ -50,11 +53,12 @@ def meeting_req(df):
                     list_optional.append(l)
         attendees = list_trainer + list_trainee
         if len(list_room) != 0:
-            attendees = attendees + list_room
+            attendees += list_room
         organizer = "ORGANIZER;CN=organiser:mailto:prameswari.kristal@nutrifood.co.id"
         fro = "prameswari.kristal@nutrifood.co.id"
 
         ddtstart = wb_main[wb_main['event_code'] == event]['event_date'].tolist()[0]
+        ddtstart += datetime.timedelta(minutes=-(7*60))
         dtend = ddtstart + datetime.timedelta(minutes= wb_main[wb_main['event_code'] == event]['event_duration'].tolist()[0])
         dtstamp = datetime.datetime.now().strftime("%Y%m%dT%H%M%SZ")
         dtstart = ddtstart.strftime("%Y%m%dT%H%M%SZ")
@@ -88,7 +92,8 @@ def meeting_req(df):
         "Tim YDL-Nutrifood"
 
         description = body1 + body2 + body3 +CRLF
-        LOC = wb_trainroom[wb_trainroom['event_code'] == event]["meeting_room"].unique().tolist()[0]
+        # LOC = wb_trainroom[wb_trainroom['event_code'] == event]["meeting_room"].unique().tolist()[0]
+        LOC = ",".join(list_room)
         attendee = ""
         for att in attendees:
             attendee += "ATTENDEE;CUTYPE=INDIVIDUAL;ROLE=REQ-PARTICIPANT;PARTSTAT=ACCEPTED;RSVP=TRUE"+CRLF+" ;CN="+att+";X-NUM-GUESTS=0:"+CRLF+" mailto:"+att+CRLF
@@ -98,9 +103,8 @@ def meeting_req(df):
         ical +="METHOD:REQUEST"+CRLF+"BEGIN:VEVENT"+CRLF+"DTSTART:"+dtstart+CRLF+"DTEND:"+dtend+CRLF+"DTSTAMP:"+dtstamp+CRLF+organizer+CRLF
         ical += "UID:FIXMEUID"+dtstamp+CRLF
         ical += attendee+"CREATED:"+dtstamp+CRLF+description+"LAST-MODIFIED:"+dtstamp+CRLF+"LOCATION:"+LOC+CRLF+"SEQUENCE:0"+CRLF+"STATUS:CONFIRMED"+CRLF
-        ical += "SUMMARY:test "+ddtstart.strftime("%Y%m%d @ %H:%M")+CRLF+"TRANSP:OPAQUE"+CRLF+"END:VEVENT"+CRLF+"END:VCALENDAR"+CRLF
+        ical += "SUMMARY: "+wb_main[wb_main['event_code'] == event]['event_name'].unique()[0]+CRLF+"TRANSP:OPAQUE"+CRLF+"END:VEVENT"+CRLF+"END:VCALENDAR"+CRLF
 
-        # eml_body = body1 + body2 + body3
         msg = MIMEMultipart('mixed')
         msg['Reply-To']=fro
         msg['Date'] = formatdate(localtime=True)
@@ -108,7 +112,8 @@ def meeting_req(df):
         msg['From'] = fro
         msg['To'] = ",".join(attendees)
         msg['CC'] = ",".join(list_optional)
-        msg['Location'] = wb_trainroom[wb_trainroom['event_code'] == event]['meeting_room'].tolist()[0]
+        # msg['Location'] = wb_trainroom[wb_trainroom['event_code'] == event]['meeting_room'].tolist()[0]
+        msg['Location'] = ",".join(list_room)
 
         part_email = MIMEText(description,"html")
         part_cal = MIMEText(ical,'calendar;method=REQUEST')
@@ -168,12 +173,14 @@ def create_db():
         db_trainee = pd.read_csv(r'database/db_trainee.csv')
         db_cc = pd.read_csv(r'database/db_cc.csv')
         db_trainroom = pd.read_csv(r'database/db_trainroom.csv')
+        db_eti = pd.read_csv(r'database/db_eti.csv')
     except:
         db_main = pd.DataFrame(columns=['event_code', 'event_id_main', 'event_id_lain', 'event_name', 'event_day', 'event_date', 'event_number', 'event_duration', 'event_category', 'eval_training_code', 'meetreq_status', 'fdh_status', 'eti_status', 'orange_status', 'report_status', 'deadline'])
         db_trainer = pd.DataFrame(columns=['event_code', 'event_name', 'trainer_name', 'trainer_email'])
         db_trainee = pd.DataFrame(columns=['event_code', 'event_name', 'eval_training_code', 'survey_id', 'trainee_name', 'NIK', 'dept', 'trainee_email', 'presensi', 'absent_remark', 'trainee_status', 'nilai_post_test', 'eti_trainer_materi', 'eti_trainer_penampilan', 'eti_trainer_interaksi', 'eti_trainer_waktu', 'eti_materi_bobot', 'eti_materi_jelas', 'eti_materi_objective', 'eti_metode_objective', 'eti_organizer', 'eti_trainee_relevan', 'eti_trainee_manfaat', 'eti_essay_1', 'eti_essay_2', 'eti_essay_3'])
         db_cc = pd.DataFrame(columns=['dept', 'atasan', 'cc_1', 'cc_2', 'cc_3', 'cc_4', 'cc_5'])
         db_trainroom = pd.DataFrame(columns=['event_code', 'event_name', 'meeting_room', 'meeting_room_email', 'meeting_room_category'])
+        db_eti = pd.DataFrame(columns=['eval_training_code', 'eti_trainer_materi', 'eti_trainer_penampilan', 'eti_trainer_interaksi', 'eti_trainer_waktu', 'eti_materi_bobot', 'eti_materi_jelas', 'eti_materi_objective', 'eti_metode', 'eti_organizer', 'eti_trainee_relevan', 'eti_trainee_manfaat', 'eti_essay_1', 'eti_essay_2', 'eti_essay_3'])
     finally:
         open_exc(path)
         db_main = pd.concat([db_main, wb_main], axis=0, ignore_index=True, sort=False)
@@ -181,18 +188,21 @@ def create_db():
         db_trainee = pd.concat([db_trainee, wb_trainee], axis=0, ignore_index=True, sort=False)
         db_cc = pd.concat([db_cc, wb_cc], axis=0, ignore_index=True, sort=False)
         db_trainroom = pd.concat([db_trainroom, wb_trainroom], axis=0, ignore_index=True, sort=False)
+        db_eti = pd.concat([db_eti, wb_eti], axis=0, ignore_index=True, sort=False)
 
         db_main.drop_duplicates(subset="event_code", keep='last', inplace=True)
         db_trainer.drop_duplicates(keep='first', inplace=True)
         db_trainee.drop_duplicates(keep='first', inplace=True)
         db_cc.drop_duplicates(keep='first', inplace=True)
         db_trainroom.drop_duplicates(keep='first', inplace=True)
+        db_eti.drop_duplicates(keep='first', inplace=True)
 
         db_main.to_csv(r'database/db_main.csv', index=False)
         db_trainer.to_csv(r'database/db_trainer.csv', index=False)
         db_trainee.to_csv(r'database/db_trainee.csv', index=False)
         db_cc.to_csv(r'database/db_cc.csv', index=False)
         db_trainroom.to_csv(r'database/db_trainroom.csv', index=False)
+        db_eti.to_csv(r'database/db_eti.csv', index=False)
 
 def training_report(df):
     for event in df[(df['meetreq_status'] == 'done') & (df['report_status'].isnull()) & (df['fdh_status'] == 'done')]['event_code'].unique():
@@ -205,7 +215,9 @@ def training_report(df):
         
         list_cc = wb_trainer[wb_trainer['event_code'] == event].loc[:,'trainer_email'].values.tolist()
         
-        fro = 'prameswari.kristal@nutrifood.co.id'
+        # fro = "prameswari.kristal@nutrifood.co.id"
+        fro = formataddr((str(Header('YDL', 'utf-8')), 'prameswari.kristal@nutrifood.co.id'))
+        rt = ["prameswari.kristal@nutrifood.co.id", "ranilia.lestari@nutrifood.co.id"]
 
         new_df1 = wb_main[wb_main['event_code'] == event]
         new_df2 = wb_trainer[wb_trainer['event_code'] == event]
@@ -244,6 +256,7 @@ def training_report(df):
         msg['To'] = ",".join(list_to)
         msg['Cc'] = ",".join(list_cc)
         msg['Subject'] = "Report {}".format(new_df['event_name'].unique()[0])
+        msg.add_header('reply-to', ",".join(rt))
         msg.attach(MIMEText(body, 'html'))
 
         idx = wb_main[wb_main['event_code'] == event].index[0]
@@ -254,26 +267,157 @@ def training_report(df):
         mailServer.starttls()
         mailServer.ehlo()
         mailServer.login(login, password)
-        mailServer.sendmail(fro, list_to+list_cc, msg.as_string())
+        mailServer.sendmail(msg['From'], list_to+list_cc, msg.as_string())
         mailServer.close()
 
         print("Report", event, "-", wb_main[wb_main['event_code'] == event]['event_name'].iloc[0], "."*(35-len(wb_main[wb_main['event_code'] == event]['event_name'].iloc[0])), "done")
 
+def eti_report(df):
+    for event in df[(df['meetreq_status'] == 'done') & (df['eti_status'].isnull()) & (df['fdh_status'] == 'done')]['event_code'].unique():
+        list_to = wb_trainer[wb_trainer['event_code'] == event].loc[:,'trainer_email'].values.tolist()
+
+        fro = 'prameswari.kristal@nutrifood.co.id'
+
+        topic = wb_main[wb_main['event_code'] == event].loc[:,'event_name'].values.tolist()[0]
+        hartang = str(wb_main[wb_main['event_code'] == event].loc[:,'event_day'].values.tolist()[0]) + " / " + str(wb_main[wb_main['event_code'] == event].loc[:,'event_date'].values.tolist()[0].strftime("%Y%m%d"))
+        wakdar = str(wb_main[wb_main['event_code'] == event].loc[:,'event_date'].values.tolist()[0].strftime("T%H%M%SZ")) + " / " + str(wb_main[wb_main['event_code'] == event].loc[:,'event_duration'].values.tolist()[0]) + " menit"
+        loc = wb_trainroom[wb_trainroom['event_code'] == event].loc[:,'meeting_room'].values[0]
+        trainer = wb_trainer[wb_trainer['event_code'] == event].loc[:,'trainer_name'].values[0]
+        sumtrainee = len(wb_trainee[wb_trainee['event_code'] == event].loc[:,'trainee_name'].values.tolist())
+
+        code = wb_main[wb_main['event_code'] == event].loc[:,'eval_training_code'].values[0]
+        mean_eti_trainer_materi = wb_eti[wb_eti['eval_training_code'] == code]['eti_trainer_materi'].mean()
+        mean_eti_trainer_penampilan = wb_eti[wb_eti['eval_training_code'] == code]['eti_trainer_penampilan'].mean()
+        mean_eti_trainer_interaksi = wb_eti[wb_eti['eval_training_code'] == code]['eti_trainer_interaksi'].mean()
+        mean_eti_trainer_waktu = wb_eti[wb_eti['eval_training_code'] == code]['eti_trainer_waktu'].mean()
+        mean_eti_materi_bobot = wb_eti[wb_eti['eval_training_code'] == code]['eti_materi_bobot'].mean()
+        mean_eti_materi_jelas = wb_eti[wb_eti['eval_training_code'] == code]['eti_materi_jelas'].mean()
+        mean_eti_materi_objective = wb_eti[wb_eti['eval_training_code'] == code]['eti_materi_objective'].mean()
+        mean_eti_metode_objective = wb_eti[wb_eti['eval_training_code'] == code]['eti_metode_objective'].mean()
+        mean_eti_organizer = wb_eti[wb_eti['eval_training_code'] == code]['eti_organizer'].mean()
+        mean_eti_trainee_relevan = wb_eti[wb_eti['eval_training_code'] == code]['eti_trainee_relevan'].mean()
+        mean_eti_trainee_manfaat = wb_eti[wb_eti['eval_training_code'] == code]['eti_trainee_manfaat'].mean()
+        all_eti_essay_1 = "\n".join(wb_eti[wb_eti['eval_training_code'] == code]['eti_essay_1'])
+        all_eti_essay_2 = "\n".join(wb_eti[wb_eti['eval_training_code'] == code]['eti_essay_2'])
+        all_eti_essay_3 = "\n".join(wb_eti[wb_eti['eval_training_code'] == code]['eti_essay_3'])
+
+        body1 = (f"Dear {trainer},<br>" \
+                "Terimakasih sudah membawakan materi training. Berikut adalah Evaluasi Training Internal dari peserta training.<br><br>" \
+                f"Topik\t\t: {topic} <br>" \
+                f"Hari/tanggal\t\t: {hartang} <br>" \
+                f"Waktu/durasi\t\t: {wakdar} <br>" \
+                f"Tempat\t\t: {loc} <br>" \
+                f"Jumlah peserta\t\t: {sumtrainee} <br>")
+        body2 = f"""<table>
+                    <tr>
+                        <th>Aspek Trainer</th>
+                        <th>Skala Nilai</th>
+                    </tr>
+                    <tr>
+                        <td>Penguasaan materi</td>
+                        <td>{mean_eti_trainer_materi}</td>
+                    </tr>
+                    <tr>
+                        <td>Penampilan & body language</td>
+                        <td>{mean_eti_trainer_penampilan}</td>
+                    </tr>
+                    <tr>
+                        <td>Kemampuan interaksi</td>
+                        <td>{mean_eti_trainer_interaksi}</td>
+                    </tr>
+                    <tr>
+                        <td>Alokasi waktu training</td>
+                        <td>{mean_eti_trainer_waktu}</td>
+                    </tr>
+                </table><br><br>"""
+        body3 = f"""<table>
+                    <tr>
+                        <th>Aspek Materi</th>
+                        <th>Skala Nilai</th>
+                    </tr>
+                    <tr>
+                        <td>Bobot</td>
+                        <td>{mean_eti_materi_bobot}</td>
+                    </tr>
+                    <tr>
+                        <td>Kejelasan</td>
+                        <td>{mean_eti_materi_jelas}</td>
+                    </tr>
+                    <tr>
+                        <td>Kesesuaian materi dgn objective training</td>
+                        <td>{mean_eti_materi_objective}</td>
+                    </tr>
+                </table><br><br>"""
+        body4 = f"""<table>
+                    <tr>
+                        <th>Aspek Metode</th>
+                        <th>Skala Nilai</th>
+                    </tr>
+                    <tr>
+                        <td>Kesesuaian metode dgn objective training</td>
+                        <td>{mean_eti_metode_objective}</td>
+                    </tr>
+                </table><br><br>"""
+        body5 = f"""<table>
+                    <tr>
+                        <th>Aspek Organizer</th>
+                        <th>Skala Nilai</th>
+                    </tr>
+                    <tr>
+                        <td>Layout, suhu & kebersihan ruangan</td>
+                        <td>{mean_eti_organizer}</td>
+                    </tr>
+                </table><br><br>"""
+        body6 = f"""<table>
+                    <tr>
+                        <th>Aspek Trainee</th>
+                        <th>Skala Nilai</th>
+                    </tr>
+                    <tr>
+                        <td>Relevansi ke pekerjaan</td>
+                        <td>{mean_eti_trainee_relevan}</td>
+                    </tr>
+                    <tr>
+                        <td>Manfaat ke pekerjaan</td>
+                        <td>{mean_eti_trainee_manfaat}</td>
+                    </tr>
+                    <tr>
+                        <td>Poin-poin penting yg bermanfaat bagi pekerjaan</td>
+                        <td>{all_eti_essay_1}</td>
+                    </tr>
+                    <tr>
+                        <td>Poin-poin yg akan diimplementasikan dalam pekerjaan</td>
+                        <td>{all_eti_essay_2}</td>
+                    </tr>
+                </table><br><br>"""
+        body7 = f"""<table>
+                    <tr>
+                        <th>Usulan Perbaikan</th>
+                    </tr>
+                    <tr>
+                        <td>{all_eti_essay_3}</td>
+                    </tr>
+                </table><br><br>"""
+
 def email_training():
+    tic = time.time()
     log_conf()
     open_exc(path)
     meeting_req(df=wb_main[wb_main['meetreq_status'] != "done"])
     extract_excel()
     create_db()
-    print("Email berhasil")
+    toc = time.time()
+    print("Email berhasil. Durasi proses: {0:.2f} detik".format(toc - tic))
 
 def email_report_training():
+    tic = time.time()
     log_conf()
     open_exc(path)
     training_report(df=wb_main)
     extract_excel()
     create_db()
-    print('Email report berhasil')
+    toc = time.time()
+    print("Email report berhasil. Durasi proses: {0:.2f} detik".format(toc - tic))
 
 class YDLapp(tk.Tk):
     def __init__(self, *args, **kwargs):
